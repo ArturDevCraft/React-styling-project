7a75461f203b914773c9ffec0c41bb116360d78b
ArturDevCraft <artur.czerwiak@gmail.com>
2026-02-18 18:04:20 +0100

    Move tooltip styles to theme

--- src/components/Button/Button.styled.jsx
+++ src/components/Button/Button.styled.jsx
@@ -1,108 +1,7 @@
 import styled, { css } from 'styled-components';
 import Icon from '../Icon';
 import variant from './variants';
-
-const tooltip = css`
-	${(props) =>
-		props.$tooltip &&
-		css`
-			overflow: visible;
-			&::after {
-				content: ${(props) => (props.$tooltip ? `"${props.$tooltip}"` : '""')};
-				display: inline-block;
-				background-color: var(--text);
-				box-shadow: ${({ theme }) => theme.shadow?.small.flat};
-				color: var(--bg);
-				position: absolute;
-				${props.$tooltipSize !== '' &&
-				css`
-					font-size: ${variant?.[props.$tooltipSize]?.fontSize};
-				`}
-				${props.$tooltipPosition === 'bottom' &&
-				css`
-					top: calc(100% + 1rem);
-					left: 50%;
-					transform: translateX(-50%);
-				`}
-				${props.$tooltipPosition === 'top' &&
-				css`
-					bottom: calc(100% + 1rem);
-					left: 50%;
-					transform: translateX(-50%);
-				`}
-
-				${props.$tooltipPosition === 'right' &&
-				css`
-					left: calc(100% + 1rem);
-					top: 50%;
-					transform: translateY(-50%);
-				`}
-				${props.$tooltipPosition === 'left' &&
-				css`
-					right: calc(100% + 1rem);
-					top: 50%;
-					transform: translateY(-50%);
-				`}
-				
-				opacity: 0;
-				transition: opacity 0.3s;
-				padding: 0.7em;
-				border-radius: 0.5em;
-				visibility: hidden;
-				text-align: center;
-				pointer-events: none;
-				z-index: 9998;
-			}
-			&::before {
-				content: '';
-				position: absolute;
-
-				border-width: 0.5rem;
-				border-style: solid;
-				opacity: 0;
-				transition: opacity 0.3s;
-				visibility: hidden;
-
-				${props.$tooltipPosition === 'bottom' &&
-				css`
-					top: 100%;
-					border-color: transparent transparent var(--text) transparent;
-					left: 50%;
-					transform: translateX(-50%);
-				`}
-				${props.$tooltipPosition === 'top' &&
-				css`
-					bottom: 100%;
-					border-color: var(--text) transparent transparent transparent;
-					left: 50%;
-					transform: translateX(-50%);
-				`}
-
-				${props.$tooltipPosition === 'right' &&
-				css`
-					left: 100%;
-					border-color: transparent var(--text) transparent transparent;
-					top: 50%;
-					transform: translateY(-50%);
-				`}
-
-				${props.$tooltipPosition === 'left' &&
-				css`
-					right: 100%;
-					border-color: transparent transparent transparent var(--text);
-					top: 50%;
-					transform: translateY(-50%);
-				`}
-
-				z-index: 9999;
-			}
-			&:hover::after,
-			&:hover::before {
-				opacity: 1;
-				visibility: visible;
-			}
-		`}
-`;
+import tooltip from '../../theme/tooltip';
 
 const StyledButton = styled.button`
 	/* default colors */
@@ -136,7 +35,7 @@ const StyledButton = styled.button`
 		color: var(--accent);
 		scale: 1.05;
 	}
-	${tooltip}
+	${tooltip(variant)}
 	&:active {
 		box-shadow: ${({ theme }) => theme.shadow?.verySmall.pressed};
 		scale: 1;
--- src/components/Slider/Slider.styled.jsx
+++ src/components/Slider/Slider.styled.jsx
@@ -1,108 +1,6 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import variant from './variants';
-
-const tooltip = css`
-	${(props) =>
-		props.$tooltip &&
-		css`
-			overflow: visible;
-			&::after {
-				content: ${(props) => (props.$tooltip ? `"${props.$tooltip}"` : '""')};
-				display: inline-block;
-				background-color: var(--text);
-				box-shadow: ${({ theme }) => theme.shadow?.small.flat};
-				color: var(--bg);
-				position: absolute;
-
-				${props.$tooltipSize !== '' &&
-				css`
-					font-size: ${variant?.[props.$tooltipSize]?.fontSize};
-				`}
-				${props.$tooltipPosition === 'bottom' &&
-				css`
-					top: calc(100% + 1rem);
-					left: 50%;
-					transform: translateX(-50%);
-				`}
-				${props.$tooltipPosition === 'top' &&
-				css`
-					bottom: calc(100% + 1rem);
-					left: 50%;
-					transform: translateX(-50%);
-				`}
-
-				${props.$tooltipPosition === 'right' &&
-				css`
-					left: calc(100% + 1rem);
-					top: 50%;
-					transform: translateY(-50%);
-				`}
-				${props.$tooltipPosition === 'left' &&
-				css`
-					right: calc(100% + 1rem);
-					top: 50%;
-					transform: translateY(-50%);
-				`}
-				
-				opacity: 0;
-				transition: opacity 0.3s;
-				padding: 0.7em;
-				border-radius: 0.5em;
-				visibility: hidden;
-				text-align: center;
-				pointer-events: none;
-				z-index: 9998;
-			}
-			&::before {
-				content: '';
-				position: absolute;
-
-				border-width: 0.5rem;
-				border-style: solid;
-				opacity: 0;
-				transition: opacity 0.3s;
-				visibility: hidden;
-
-				${props.$tooltipPosition === 'bottom' &&
-				css`
-					top: 100%;
-					border-color: transparent transparent var(--text) transparent;
-					left: 50%;
-					transform: translateX(-50%);
-				`}
-				${props.$tooltipPosition === 'top' &&
-				css`
-					bottom: 100%;
-					border-color: var(--text) transparent transparent transparent;
-					left: 50%;
-					transform: translateX(-50%);
-				`}
-
-				${props.$tooltipPosition === 'right' &&
-				css`
-					left: 100%;
-					border-color: transparent var(--text) transparent transparent;
-					top: 50%;
-					transform: translateY(-50%);
-				`}
-
-				${props.$tooltipPosition === 'left' &&
-				css`
-					right: 100%;
-					border-color: transparent transparent transparent var(--text);
-					top: 50%;
-					transform: translateY(-50%);
-				`}
-
-				z-index: 9999;
-			}
-			&:hover::after,
-			&:hover::before {
-				opacity: 1;
-				visibility: visible;
-			}
-		`}
-`;
+import tooltip from '../../theme/tooltip';
 
 const sliderThumbStyles = `
 		appearance: none;
@@ -168,7 +66,7 @@ const StyledSliderWrapper = styled.div`
 	&:hover {
 		scale: 1.1;
 	}
-	${tooltip}
+	${tooltip(variant)}
 	${({ size }) => variant?.[size] ?? variant.medium}
 	${({ shape }) => variant?.[shape]}
 	${({ width, size }) =>
