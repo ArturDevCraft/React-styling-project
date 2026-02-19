dd597644d96529f42cfb4c1b8942d831e478bf0b
ArturDevCraft <artur.czerwiak@gmail.com>
2026-02-19 18:00:30 +0100

    Add alignment

--- src/components/RowWrapper.styled.jsx
+++ src/components/RowWrapper.styled.jsx
@@ -1,10 +1,30 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const StyledRowWrapper = styled.div`
 	display: flex;
 	flex-direction: row;
-	justify-content: space-around;
+
 	gap: 2rem;
+	${(props) =>
+		props?.$align === 'right' &&
+		css`
+			justify-content: flex-end;
+		`};
+	${(props) =>
+		props?.$align === 'left' &&
+		css`
+			justify-content: flex-start;
+		`};
+	${(props) =>
+		props?.$align === 'center' &&
+		css`
+			justify-content: center;
+		`};
+	${(props) =>
+		props?.$align === 'justify' &&
+		css`
+			justify-content: space-between;
+		`};
 `;
 
 export default StyledRowWrapper;
