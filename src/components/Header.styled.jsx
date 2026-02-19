cfd45ccf63853b45bf81b809294ba2d5308224b2
ArturDevCraft <artur.czerwiak@gmail.com>
2026-02-19 16:12:00 +0100

    Improve styles

--- src/components/Header.styled.jsx
+++ src/components/Header.styled.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 const StyledHeader = styled.header`
 	display: flex;
 	justify-content: space-between;
+	align-items: flex-start;
 `;
 
 export default StyledHeader;
