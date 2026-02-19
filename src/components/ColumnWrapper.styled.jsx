f2f9c34b9601bc43709a2031648517ac7c5e8812
ArturDevCraft <artur.czerwiak@gmail.com>
2026-02-19 17:33:40 +0100

    Add alignment

--- src/components/ColumnWrapper.styled.jsx
+++ src/components/ColumnWrapper.styled.jsx
@@ -1,9 +1,24 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const StyledColumnWrapper = styled.div`
 	display: flex;
 	flex-direction: column;
 	justify-content: flex-start;
+	${(props) =>
+		props?.align === 'right' &&
+		css`
+			align-items: flex-end;
+		`};
+	${(props) =>
+		props?.align === 'left' &&
+		css`
+			align-items: flex-start;
+		`};
+	${(props) =>
+		props?.align === 'center' &&
+		css`
+			align-items: center;
+		`};
 `;
 
 export default StyledColumnWrapper;
