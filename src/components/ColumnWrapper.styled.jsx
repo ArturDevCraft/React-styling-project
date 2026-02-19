8cb86f879fbf497e2fa230ab38bd1174958c6a9c
ArturDevCraft <artur.czerwiak@gmail.com>
2026-02-19 16:11:19 +0100

    Create Column and Row wrapper Components

---
+++ src/components/ColumnWrapper.styled.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const StyledColumnWrapper = styled`
+display: flex;
+flex-direction: column;
+justify-content: flex-start;
+`;
+
+export default StyledColumnWrapper;
---
+++ src/components/RowWrapper.styled.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const StyledRowWrapper = styled.section`
+	display: flex;
+	flex-direction: row;
+	justify-content: space-around;
+	gap: 2rem;
+`;
+
+export default StyledRowWrapper;
