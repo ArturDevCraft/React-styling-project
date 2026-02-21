0a3f06a7e55b6fd72a7a4b40a5c1c8d74e569b99
ArturDevCraft <artur.czerwiak@gmail.com>
2026-02-21 16:39:00 +0100

    Add switching steps

--- src/components/HireMe/HireMe.jsx
+++ src/components/HireMe/HireMe.jsx
@@ -4,13 +4,19 @@ import StyledWrapper from '../Wrapper.styled';
 import StyledHorizontalLine from '../HorizontalLine.styled';
 import StyledRowWrapper from '../RowWrapper.styled';
 import StyledColumnWrapper from '../ColumnWrapper.styled';
-import { useContext, useState } from 'react';
+import { useEffect, useState } from 'react';
 import Header from './Header';
 
 import Skills from './Skills';
+import Button from '../Button';
+import Icon from '../Icon';
 
 export default function HireMe() {
 	const [step, setStep] = useState(1);
+	useEffect(() => {
+		if (step < 1) setStep(1);
+		else if (step > 3) setStep(3);
+	}, [step]);
 
 	return (
 		<StyledWrapper>
@@ -23,7 +29,28 @@ export default function HireMe() {
 					<RadioPlayer url="https://stream.rcs.revma.com/ye5kghkgcm0uv" />
 				</StyledColumnWrapper>
 				<StyledColumnWrapper style={{ flexGrow: 2 }}>
-					<Skills step={step} />
+					{step === 1 && <Skills step={step} />}
+					<StyledHorizontalLine />
+					<StyledRowWrapper $align="justify">
+						<Button
+							type="cta"
+							size="small"
+							onClick={() => setStep(step - 1)}
+							disabled={step === 1}
+						>
+							<Icon type="arrowLeft" style={{ fontSize: '1.8em' }} />
+							Previous step
+						</Button>
+						<Button
+							type="cta"
+							size="small"
+							onClick={() => setStep(step + 1)}
+							disabled={step === 3}
+						>
+							Next step
+							<Icon type="arrowRight" style={{ fontSize: '1.8em' }} />
+						</Button>
+					</StyledRowWrapper>
 				</StyledColumnWrapper>
 			</StyledRowWrapper>
 		</StyledWrapper>
